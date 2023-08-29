b8463ed6dff49fe0a9519d07a35c15e3bb3fb312
Rachel Tenenbaum <racheltenenbaum14@gmail.com>
2023-08-29 12:51:25 +0100

    added new user form and user seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :location, :description, :birth_year, :gender)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(email: "luke@luke.com", password: "123456", name: "Luke", location:"London", description: "I am Luke from London", birth_year: 1990, gender: "male")
+User.create(email: "mary@mary.com", password: "123456", name: "Mary", location:"Paris", description: "I am Mary from Paris", birth_year: 1992, gender: "female")
+User.create(email: "phil@phil.com", password: "123456", name: "Phil", location:"Manchester", description: "I am Phil from Manchester", birth_year: 1991, gender: "male")
+User.create(email: "leah@leah.com", password: "123456", name: "Leah", location:"Rome", description: "I am Leah from Rome", birth_year: 1994, gender: "female")
