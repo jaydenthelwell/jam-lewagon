9a40efdf9d416c35f6ee7e832f2ffb790d5858e4
Jayden Thelwell <jaydenthelwell02@gmail.com>
2023-09-01 12:10:29 +0100

    Just made the registrations controller to avoid conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   # Defines the root path route ("/")
   # root "articles#index"
