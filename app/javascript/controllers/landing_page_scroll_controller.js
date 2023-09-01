b1f6196c8c139eb9ebd31e77619039238c8241b8
Le Wagon <lissy.gray@aol.co.uk>
2023-09-01 14:54:52 +0100

    hey gang

--- app/javascript/controllers/landing_page_scroll_controller.js
+++ app/javascript/controllers/landing_page_scroll_controller.js
@@ -5,23 +5,25 @@ export default class extends Controller {
   static targets = ["secondPage"]
 
   connect() {
-    console.log("Hello from landing page scoll")
+    console.log("Hello from landing page scroll");
   }
 
   scrollUp() {
-    console.log("Scrolling up ...")
+    console.log("Scrolling up ...");
 
-    console.log(this.secondPageTarget)
-    this.secondPageTarget.classList.add("down")
-    this.secondPageTarget.classList.remove("up")
+    console.log(this.secondPageTarget);
+
+    this.secondPageTarget.classList.add("down");
+    this.secondPageTarget.classList.remove("up");
   }
 
   scrollDown() {
-    console.log("Scrolling down ...")
+    console.log("Scrolling down ...");
 
-    console.log(this.secondPageTarget)
-    this.secondPageTarget.classList.add("up")
-    this.secondPageTarget.classList.remove("down")
+    console.log(this.secondPageTarget);
+
+    this.secondPageTarget.classList.add("up");
+    this.secondPageTarget.classList.remove("down");
   }
 }
 
