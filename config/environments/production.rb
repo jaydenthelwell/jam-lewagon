1f0d06c7ce47d4ffab2105ee2f221cfff655c3ac
Jurgest Hajrullaku <hajrullakuj@gmail.com>
2023-09-04 11:18:55 +0100

    probably fixed chatroom

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://https://jam-with-me-39ed82aebee1.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://jam-with-me-39ed82aebee1.herokuapp.com/", /http:\/\/jam-with-me-39ed82aebee1.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
