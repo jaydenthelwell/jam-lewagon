f6630692808aa8acb131da65476b1e84db638078
Fahim Razzaq <famraz@gmail.com>
2023-09-04 12:26:39 +0100

    attaching many photos on profile instead of one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :date_of_birth, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :date_of_birth, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, photos: []])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :date_of_birth, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :date_of_birth, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, photos: []])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
-
-
   devise_for :users, controllers: { registrations: "registrations" }
 
   # Defines the root path route ("/")
