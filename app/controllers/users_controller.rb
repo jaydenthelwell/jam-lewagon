58e7dec9045f34ff02e2746cfbfe45cac02421d5
Jurgest Hajrullaku <hajrullakuj@gmail.com>
2023-09-05 14:15:07 +0100

    added the spotify in the sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,14 +27,14 @@ class UsersController < ApplicationController
   def profile
   end
 
-  # def new
-  #   @user = User.new
-  # end
+  def new
+    @user = User.new
+  end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   @user.save
-  # end
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
 
   def like
     if current_user.like(@user.id)
