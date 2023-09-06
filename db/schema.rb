753b34c290476b4fed3e6e5cd1f1d96a84efe469
Rachel Tenenbaum <racheltenenbaum14@gmail.com>
2023-09-06 15:21:54 +0100

    progress bar implemented

--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_100256) do
   end
 
   create_table "chatrooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "match_id"
+    t.index ["match_id"], name: "index_chatrooms_on_match_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
     t.bigint "swipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["swipe_id"], name: "index_chatrooms_on_swipe_id"
+    t.index ["swipe_id"], name: "index_matches_on_swipe_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -121,7 +128,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_100256) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "chatrooms", "swipes"
+  add_foreign_key "matches", "swipes"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "photos", "socials"
