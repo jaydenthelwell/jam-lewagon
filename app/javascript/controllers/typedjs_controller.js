af0bcdd010c50515c720a78def744fda2c989d9f
Le Wagon <lewagon@Les-MacBook-Pro-2.local>
2023-09-06 09:46:05 +0100

    landing page

--- app/javascript/controllers/typedjs_controller.js
+++ app/javascript/controllers/typedjs_controller.js
@@ -6,11 +6,11 @@ export default class extends Controller {
   connect() {
     console.log("Hello from typedjs")
 
-    new Typed(this.element, {
+   new Typed(this.element, {
       strings: ['Jam'],
       typeSpeed: 75,
       startDelay: 100
-    })
+   })
 
     document.querySelector(".typed-cursor").classList.add("d-none")
   }
